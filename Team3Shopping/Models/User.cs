66ec4e28d976410f685f81b37c6c375dd18e0b60
Manuel Seet <e0838385@u.nus.edu>
2021-10-13 16:12:26 +0800

    Added User model

---
+++ Team3Shopping/Models/User.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Team3Shopping.Models
+{
+    public class User
+    {
+        public User()
+        {
+        }
+
+        [Required]
+        [Key]
+        [MaxLength(32)]
+        public string Username { get; set; }
+
+        [Required]
+        [MaxLength(32)]
+        public string Password { get; set; }
+
+        [Required]
+        [MaxLength(32)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MaxLength(32)]
+        public string LastName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [MaxLength(32)]
+        public string Email { get; set; }
+
+    }
+}
