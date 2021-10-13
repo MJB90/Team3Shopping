5d4c3314e86404807f9ffc3588fa751389afb97b
Manuel Seet <e0838385@u.nus.edu>
2021-10-13 17:45:47 +0800

    Added myDBContext

---
+++ Team3Shopping/Models/myDBContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Team3Shopping.Models
+{
+    public class myDBContext : DbContext
+    {
+        public myDBContext(DbContextOptions<myDBContext> options) : base (options)
+        { }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
