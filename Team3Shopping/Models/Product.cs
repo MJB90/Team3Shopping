08c0f0e59344cfe8d7a2497fd5f26aeea23decc1
Manuel Seet <e0838385@u.nus.edu>
2021-10-13 17:31:14 +0800

    Added Product model

---
+++ Team3Shopping/Models/Product.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace Team3Shopping.Models
+{
+    public class Product
+    {
+        public Product()
+        {
+            ProductId = new Guid();
+        }
+
+        [Key]
+        public Guid ProductId { get; set; }
+
+        [Required]
+        [MaxLength(32)]
+        public string ProductName { get; set; }
+
+        [Required]
+        [MaxLength(150)]
+        public string ProductDesc { get; set; }
+
+        [Required]
+        public float ProductPrice { get; set; }
+
+        [Required]
+        [MaxLength(32)]
+        public string ProductImageFileName { get; set; }
+
+        [Required]
+        [MaxLength(32)]
+        public string ProductCategory { get; set; }
+    }
+}
