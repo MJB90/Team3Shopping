e65aec8fd40f1bcf9137110730c676fb9105a09a
Gabriel Wei Chen Kho <e0838413@u.nus.edu>
2021-10-14 16:02:18 +0800

    Adding Session.cs

---
+++ Team3Shopping/Models/Session.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Team3Shopping.Models
+{
+    public class Session
+    {
+        public Session()
+        {
+            Id = new Guid();
+            Timestamp = DateTimeOffset.Now.ToUnixTimeSeconds();
+        }
+        public Guid Id { get; set; }
+        [Required]
+        public long Timestamp { get; set; }
+
+        public virtual Guid UserId { get; set; }
+
+        public virtual User User { get; set; }
+    }
+}
+
