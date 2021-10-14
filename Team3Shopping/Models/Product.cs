7ed453308e7ca300cf863857ede6feaab642ef94
Gabriel Wei Chen Kho <e0838413@u.nus.edu>
2021-10-14 16:13:49 +0800

    edit

--- Team3Shopping/Models/Product.cs
+++ Team3Shopping/Models/Product.cs
@@ -14,5 +14,26 @@ namespace Team3Shopping.Models
             Id = new Guid();
             Cart = new List<Cart>();
             PurchaseProduct = new List<PurchaseProduct>();
-       
+        }
+        public Guid Id { get; set; }
+        [Required]
+        [MaxLength(32)]
+        public string ProductName { get; set; }
+        [Required]
+        [MaxLength(256)]
+        public string ProductDesc { get; set; }
+
+        [Required]
+        public float ProductPrice { get; set; }
+        [Required]
+        public string ProductImage { get; set; }
+        [Required]
+        [MaxLength(32)]
+        public string ProductCategory { get; set; }
+        [Required]
+        public int ProductQuantity { get; set; }
+
+        public virtual ICollection<Cart> Cart { get; set; }
+        public virtual ICollection<PurchaseProduct> PurchaseProduct { get; set; }
+    }
 }
