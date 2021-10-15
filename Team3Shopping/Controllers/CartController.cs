1c6f39a1d4f4179924a979c212290cf258c7d7a1
Gabriel Wei Chen Kho <e0838413@u.nus.edu>
2021-10-15 09:45:05 +0800

    Cart

--- Team3Shopping/Controllers/CartController.cs
+++ Team3Shopping/Controllers/CartController.cs
@@ -17,7 +17,37 @@ namespace Team3Shopping.Controllers
         //This action will bring u to the Cart page 
         public IActionResult ToCart()
         {
-            
+
+            //Select all the products in the cart
+            List<Cart> carts = dbContext.Carts.Where(x =>
+                x.UserId != null && x.ProductId != null).ToList();
+            List<Product> products = new List<Product>();
+            foreach (Cart c in carts)
+            {
+                Product product = dbContext.Products.FirstOrDefault(x =>
+                  x.Id == c.ProductId);
+                if (product != null)
+                {
+                    products.Add(product);
+                }
+            }
+
+            //Select all the users who have added some products to the cart
+            List<User> users = new List<User>();
+            foreach (Cart c in carts)
+            {
+                User user = dbContext.Users.FirstOrDefault(x =>
+                  x.Id == c.UserId);
+                if (user != null)
+                {
+                    users.Add(user);
+                }
+            }
+
+
+            ViewData["product"] = products;
+            ViewData["user"] = users;
+            ViewData["cart"] = carts;
             return View();
         }
     }
