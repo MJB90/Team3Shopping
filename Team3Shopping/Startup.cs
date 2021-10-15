130006b80df86f22614a221126c5da7f0b6f22ef
Gabriel Wei Chen Kho <e0838413@u.nus.edu>
2021-10-15 14:58:17 +0800

    Change the landing page to Login page

--- Team3Shopping/Startup.cs
+++ Team3Shopping/Startup.cs
@@ -59,7 +59,7 @@ namespace Team3Shopping
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Login}/{action=Index}/{id?}");
             });
 
             if (!dbContext.Database.CanConnect())
