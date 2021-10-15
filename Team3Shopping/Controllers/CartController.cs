c3b676c9dd62b836acf4d0d33773dddb0df59b3d
Gabriel Wei Chen Kho <e0838413@u.nus.edu>
2021-10-15 14:49:10 +0800

    Add in remove and change quantity functionality

--- Team3Shopping/Controllers/CartController.cs
+++ Team3Shopping/Controllers/CartController.cs
@@ -14,11 +14,11 @@ namespace Team3Shopping.Controllers
         {
             this.dbContext = dbContext;
         }
-        //This action will bring u to the Cart page 
+        //Redirect to cart page 
         public IActionResult ToCart()
         {
 
-            //Select all the products in the cart
+            //Select all products in the cart
             List<Cart> carts = dbContext.Carts.Where(x =>
                 x.UserId != null && x.ProductId != null).ToList();
             List<Product> products = new List<Product>();
@@ -32,7 +32,7 @@ namespace Team3Shopping.Controllers
                 }
             }
 
-            //Select all the users who have added some products to the cart
+            //Select all users who have added some products to the cart
             List<User> users = new List<User>();
             foreach (Cart c in carts)
             {
@@ -48,6 +48,57 @@ namespace Team3Shopping.Controllers
             ViewData["user"] = users;
             ViewData["cart"] = carts;
             return View();
+        }
+
+        public IActionResult Remove( string UserId , string ProductId , string AddToCartProductQuantity)
+        {
+            Guid productId = Guid.Parse(ProductId);
+            //Update carts database
+            Cart cart = dbContext.Carts.FirstOrDefault(x =>
+              x.ProductId == productId && x.UserId == UserId);
+            cart.AddToCartProductQuantity -= Convert.ToInt32(AddToCartProductQuantity);
+
+            //Update products database
+            Product product = dbContext.Products.FirstOrDefault(x =>
+              x.Id == productId);
+            product.ProductQuantity += Convert.ToInt32(AddToCartProductQuantity);
+
+            dbContext.SaveChanges();
+            return Json(new { successfullyUpdateDatabase = true });
+        }
+        //Update products and carts database
+        public IActionResult EditQuantity(string UserId, string ProductId, string ProductQuantity)
+        {
+            Product product = dbContext.Products.FirstOrDefault(x => x.Id == Guid.Parse(ProductId));
+
+            Cart cart = dbContext.Carts.FirstOrDefault(x =>
+            x.UserId == UserId && x.ProductId == Guid.Parse(ProductId));
+
+            product.ProductQuantity += (cart.AddToCartProductQuantity - Convert.ToInt32(ProductQuantity));
+            cart.AddToCartProductQuantity -= (cart.AddToCartProductQuantity - Convert.ToInt32(ProductQuantity));
+
+            dbContext.SaveChanges();
+            return Json(new { data = true  });
+        }
+
+        //Redirect to the purhase is been made page
+        public IActionResult PurchaseDone()
+        {
+            //Do some validations of the payment
+            //Omitted for brevity
+            return View();
+        }
+
+        private Session GetSession()
+        {
+            if (Request.Cookies["SessionId"] == null)
+            {
+                return null;
+            }
+            Guid sessionId = Guid.Parse(Request.Cookies["SessionId"]);
+            Session session = dbContext.Sessions.FirstOrDefault(x =>
+              x.Id == sessionId);
+            return session;
         }
     }
 }
--- Team3Shopping/wwwroot/js/cartPage.js
+++ Team3Shopping/wwwroot/js/cartPage.js
@@ -40,3 +40,75 @@ $(document).ready(function () {
         $(this).removeClass('gray');
     });
 });
+
+
+//Remove products form the cart
+window.onload = function () {
+    let elems = document.getElementsByClassName("item");
+    for (let i = 0; i < elems.length; i++) {
+        elems[i].childNodes[3].lastChild.addEventListener('click', RemoveProduct);
+        elems[i].childNodes[3].childNodes[1].addEventListener('change', ChangeQuantity);
+    }
+}
+
+function RemoveProduct(event) {
+    let target = event.currentTarget;
+    let number = document.getElementById(target.className.substring(7)).value;
+    checkIsProductDeleted(target.id, target.className.substring(7),number);
+    target.remove();
+    updateTotalPrice();
+}
+
+function ChangeQuantity(event) {
+    targetNum = event.currentTarget;
+    let number = document.getElementById(target.className.substring(7)).value;
+    let user = targetNum.id;
+    let product = target.className.substring(7);
+    ChangeQuantityInDB(user, product, number);
+    if (number === 0) {
+        targetNum.remove();
+    }
+    updateCartTotal();
+}
+
+function checkIsProductDeleted(userId, productId, addToCartProductQuantity) {
+    $.ajax({
+        type: "POST",
+        url: '@Url.Action("Remove","Cart")',
+        data: {
+            UserId: userId,
+            ProductId: productId,
+            AddToCartProductQuantity: addToCartProductQuantity
+        },
+        dataType: "json",
+        success: function () {
+            alert("Product has been removed from the cart!")
+        },
+        error: function () {
+            alert("Something went wrong!")
+        }
+    })
+}
+
+function ChangeQuantityInDB(userId, productId, productQuantity) {
+    $.ajax({
+        type: "POST",
+        url: '@Url.Action("EditQuantity", "Cart")',
+        data: {
+            UserId: userId,
+            ProductId: productId,
+            ProductQuantity: productQuantity
+        },
+        dataType: "json",
+        success: function () {
+            alert("success")
+        },
+        error: function () {
+            alert("error")
+        }
+    })
+}
+
+function updateTotalPrice() {
+    
+}
